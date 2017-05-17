7dcb8e161c17580ff154f6646abe669b73868fff
Kyle Nathan <kyle@wearelion.com>
2017-05-17 17:58:33 +0100

    Fix missing config handling

--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,8 @@
 'use strict';
 
 module.exports = function(environment, appConfig) {
-  appConfig['APP']['goods']['host'] = 'https://api.goods.co.uk' || appConfig['APP']['goods']['host'];
+  appConfig['APP']['goods'] = {
+    host: 'https://api.goods.co.uk' || appConfig['goods']['host']
+  };
   return {};
 };
