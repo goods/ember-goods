d749325fe44adf91f287c9dda9e187cefa2d1e93
Kyle Nathan <kyle@wearelion.com>
2017-06-20 10:26:39 +0100

    Add order model

---
+++ addon/models/order.js
@@ -0,0 +1,22 @@
+import DS from 'ember-data';
+const { Model, belongsTo, attr } = DS;
+
+export default Model.extend({
+  basket: belongsTo('basket'),
+  name: attr('string'),
+  emailAddress: attr('string'),
+  phoneNumber: attr('string'),
+  mobileNumber: attr('string'),
+  billingAddress1: attr('string'),
+  billingAddress2: attr('string'),
+  billingCity: attr('string'),
+  billingRegion: attr('string'),
+  billingPostcode: attr('string'),
+  billingCountry: attr('string'),
+  shippingAddress1: attr('string'),
+  shippingAddress2: attr('string'),
+  shippingCity: attr('string'),
+  shippingRegion: attr('string'),
+  shippingPostcode: attr('string'),
+  shippingCountry: attr('string'),
+});
