ea48dfe375006d1871cddebc7c1917e009bb85d5
Kyle Nathan <kyle@wearelion.com>
2017-06-20 10:27:16 +0100

    Add basket-item model

---
+++ addon/models/basket-item.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+const { Model, attr, belongsTo } = DS;
+
+export default Model.extend({
+  basket: belongsTo('basket'),
+  sku: belongsTo('sku'),
+  quantity: attr('number'),
+  price: attr('number'),
+});
