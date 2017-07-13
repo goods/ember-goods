f452a59beb915db9041a4475a6e1da23308e3740
Kyle Nathan <kyle@wearelion.com>
2017-07-13 14:29:52 +0100

    Inject services into application

--- addon/initializers/goods.js
+++ addon/initializers/goods.js
@@ -1,21 +1,27 @@
-import GoodsAdapter from '../adapters/goods';
-import GoodsSerializer from '../serializers/goods';
+import GoodsAdapter from "../adapters/goods";
+import GoodsSerializer from "../serializers/goods";
+import BasketService from "../services/basket";
+import ProductService from "../services/product";
+import SkuService from "../services/sku";
 
-export function initialize( application ) {
-  let config = application.get( 'goods' );
+export function initialize(application) {
+  let config = application.get("goods");
   GoodsAdapter.reopen({
     headers: {
-      'Accept': 'application/vnd.api+json',
-      'content-type': 'application/vnd.api+json;',
-      'Authorization': 'Bearer ' + config.access_token
+      Accept: "application/vnd.api+json",
+      "content-type": "application/vnd.api+json;",
+      Authorization: "Bearer " + config.access_token
     },
     host: config.host
   });
-  application.register('adapter:application', GoodsAdapter);
-  application.register('serializer:application', GoodsSerializer);
+  application.register("adapter:application", GoodsAdapter);
+  application.register("serializer:application", GoodsSerializer);
+  application.register("service:basket", BasketService);
+  application.register("service:product", ProductService);
+  application.register("service:sku", SkuService);
 }
 
 export default {
-  name: 'goods',
+  name: "goods",
   initialize
 };
