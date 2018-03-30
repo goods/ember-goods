3bff0f2d736cae5dc1edbd86a30903346a3de171
Kyle Nathan <kyle@wearelion.com>
2018-03-30 12:53:46 +0100

    Register commerce model adapters in initializer

--- addon/initializers/goods.js
+++ addon/initializers/goods.js
@@ -16,6 +16,28 @@ export function initialize(application) {
   });
   application.register("adapter:application", GoodsAdapter);
   application.register("serializer:application", GoodsSerializer);
+
+  application.register("adapter:basket-item", GoodsAdapter);
+  application.register("adapter:basket", GoodsAdapter);
+  application.register("adapter:brand", GoodsAdapter);
+  application.register("adapter:category", GoodsAdapter);
+  application.register("adapter:country", GoodsAdapter);
+  application.register("adapter:order-line", GoodsAdapter);
+  application.register("adapter:order-payment-method", GoodsAdapter);
+  application.register("adapter:order", GoodsAdapter);
+  application.register("adapter:payment-method", GoodsAdapter);
+  application.register("adapter:payment", GoodsAdapter);
+  application.register("adapter:product-category", GoodsAdapter);
+  application.register("adapter:product-field", GoodsAdapter);
+  application.register("adapter:product-image", GoodsAdapter);
+  application.register("adapter:product", GoodsAdapter);
+  application.register("adapter:promotion", GoodsAdapter);
+  application.register("adapter:shop-payment-method", GoodsAdapter);
+  application.register("adapter:sku-field", GoodsAdapter);
+  application.register("adapter:sku-image", GoodsAdapter);
+  application.register("adapter:sku", GoodsAdapter);
+  application.register("adapter:state", GoodsAdapter);
+
   application.register("service:basket", BasketService);
   application.register("service:product", ProductService);
   application.register("service:sku", SkuService);
