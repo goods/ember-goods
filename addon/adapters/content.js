ed800882e327157bf7eff2ec2b1df10b81e0cec2
Kyle Nathan <kyle@wearelion.com>
2018-09-06 10:53:51 +0100

    Use /content/ in adapter pathForType

--- addon/adapters/content.js
+++ addon/adapters/content.js
@@ -1,13 +1,8 @@
-import EmberError from "@ember/error";
-import { dasherize } from "@ember/string";
-import { pluralize } from "ember-inflector";
 import JSONAPIAdapter from "ember-data/adapters/json-api";
-import { assign } from "@ember/polyfills";
 
 export default JSONAPIAdapter.extend({
   coalesceFindRequests: true,
 
-  host: "",
   modelWhitelist: [
     "basket",
     "basket-item",
@@ -33,113 +28,7 @@ export default JSONAPIAdapter.extend({
     if (this.modelWhitelist.includes(type)) {
       return this._super(...arguments);
     }
-    return "entries";
-  },
 
-  /**
-   @override
-   */
-  findAll(store, type, sinceToken) {
-    let modelName = type.modelName;
-
-    if (this.modelWhitelist.includes(type.modelName)) {
-      return this._super(...arguments);
-    }
-
-    let query = {
-      content_group_api_identifier: pluralize(dasherize(modelName))
-    };
-
-    const url = this.buildURL(modelName, null, null, "findAll");
-
-    if (sinceToken) {
-      query.since = sinceToken;
-    }
-
-    if (this.sortQueryParams) {
-      query = this.sortQueryParams(query);
-    }
-    return this.ajax(url, "GET", { data: { filter: query } });
-  },
-
-  // /**
-  //  @override
-  //  */
-  query(store, type, query) {
-    let modelName = type.modelName;
-
-    if (this.modelWhitelist.includes(type.modelName)) {
-      return this._super(...arguments);
-    }
-
-    query.filter = assign({}, query.filter, {
-      content_group_api_identifier: pluralize(dasherize(modelName))
-    });
-
-    if (this.sortQueryParams) {
-      query = this.sortQueryParams(query);
-    }
-
-    let url = this.buildURL(modelName, null, null, "query", query);
-
-    return this.ajax(url, "GET", { data: query });
-  },
-
-  /**
-   @override
-   * */
-  queryRecord() {
-    throw new EmberError(
-      "You may not call 'queryRecord' on a store. Use 'query'."
-    );
-  },
-  /**
-   @override
-   */
-  findMany(store, type) {
-    if (this.modelWhitelist.includes(type.modelName)) {
-      return this._super(...arguments);
-    }
-    return null;
-  },
-
-  /**
-   @override
-   */
-  findHasMany(store, type) {
-    if (this.modelWhitelist.includes(type.modelName)) {
-      return this._super(...arguments);
-    }
-    return null;
-  },
-
-  /**
-   @override
-   */
-  findBelongsTo(store, type) {
-    if (this.modelWhitelist.includes(type.modelName)) {
-      return this._super(...arguments);
-    }
-    return null;
-  },
-
-  /**
-   @override
-   */
-  updateRecord(store, type) {
-    if (this.modelWhitelist.includes(type.modelName)) {
-      return this._super(...arguments);
-    }
-    throw new EmberError("You may not call 'updateRecord' on a content entry.");
-  },
-
-  /**
-   @override
-   * */
-  deleteRecord(store, type) {
-    if (this.modelWhitelist.includes(type.modelName)) {
-      return this._super(...arguments);
-    }
-    throw new EmberError("You may not call 'deleteRecord' on a content entry.");
+    return "content/" + this._super(...arguments);
   }
 });
