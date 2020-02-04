1d0766b70b0ecb01745108de1a5ac58d58e32d7e
Kyle Nathan <kyle@wearelion.com>
2020-02-04 16:41:03 +0000

    Update ember-simple-auth

--- addon/adapters/goods.js
+++ addon/adapters/goods.js
@@ -2,6 +2,7 @@ import JSONAPIAdapter from "ember-data/adapters/json-api";
 import DataAdapterMixin from "ember-simple-auth/mixins/data-adapter-mixin";
 import { isNone } from "@ember/utils";
 import config from "ember-get-config";
+import { computed } from "@ember/object";
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
   coalesceFindRequests: true,
@@ -44,14 +45,12 @@ export default JSONAPIAdapter.extend(DataAdapterMixin, {
     return "content/" + this._super(...arguments);
   },
 
-  authorize(xhr) {
+  headers: computed("session.data.authenticated.access_token", function() {
     //Support legacy access_token
     if (!isNone(config.APP.goods.access_token)) {
-      xhr.setRequestHeader(
-        "Authorization",
-        `Bearer ${config.APP.goods.access_token}`
-      );
-      return;
+      return {
+        Authorization: `Bearer ${config.APP.goods.access_token}`
+      };
     }
 
     let { access_token } = this.get("session.data.authenticated");
@@ -59,7 +58,9 @@ export default JSONAPIAdapter.extend(DataAdapterMixin, {
     if (isNone(access_token)) {
       access_token = config.APP.goods.apiKey;
     }
-    xhr.setRequestHeader("Authorization", `Bearer ${access_token}`);
-    xhr.setRequestHeader("Space-ID", config.APP.goods.spaceId);
-  }
+    return {
+      Authorization: `Bearer ${access_token}`,
+      "Space-ID": config.APP.goods.spaceId
+    };
+  })
 });
