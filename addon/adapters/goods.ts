810a09b9378af045da8ed670fd67c7a5661008dc
Kyle Nathan <kyle@wearelion.com>
2020-09-04 17:00:52 +0100

    Update to ember-simple-auth 3.0.0

---
+++ addon/adapters/goods.ts
@@ -0,0 +1,99 @@
+import JSONAPIAdapter from "@ember-data/adapter/json-api";
+import DataAdapterMixin from "ember-simple-auth/mixins/data-adapter-mixin";
+import { isNone } from "@ember/utils";
+import config from "ember-get-config";
+import { computed } from "@ember/object";
+import { inject } from "@ember/service";
+
+export default class ApplicationAdapter extends JSONAPIAdapter.extend(
+  DataAdapterMixin,
+  {
+    pathForType(type) {
+      console.log(this.modelWhitelist);
+      if (this.modelWhitelist.includes(type)) {
+        return this._super(...arguments);
+      }
+      return "content/" + this._super(...arguments);
+    },
+  }
+) {
+  coalesceFindRequests = true;
+  host = config.APP.goods.host;
+
+  modelWhitelist = [
+    "basket",
+    "basket-item",
+    "brand",
+    "category",
+    "country",
+    "content-entry-operation",
+    "order",
+    "order-line",
+    "order-payment-method",
+    "payment",
+    "payment-method",
+    "price",
+    "product-category",
+    "product-field",
+    "product-image",
+    "product",
+    "promotion",
+    "shop-member",
+    "shop-password-change",
+    "shop-payment-method",
+    "shop-reset-token",
+    "shop-role",
+    "state",
+    "sku-field",
+    "sku-image",
+    "sku",
+    "user",
+  ];
+
+  @inject session;
+
+  @computed("session.data.authenticated.access_token")
+  get headers() {
+    //Support legacy access_token
+    if (!isNone(config.APP.goods.access_token)) {
+      return {
+        Authorization: `Bearer ${config.APP.goods.access_token}`,
+      };
+    }
+
+    let { access_token } = this.get("session.data.authenticated");
+
+    if (isNone(access_token)) {
+      access_token = config.APP.goods.apiKey;
+    }
+    return {
+      Authorization: `Bearer ${access_token}`,
+      "Space-ID": config.APP.goods.spaceId,
+    };
+
+    // if (this.session.isAuthenticated) {
+    //   headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
+    // }
+
+    // return headers;
+  }
+
+  // headers: computed("session.data.authenticated.access_token", function() {
+  //   //Support legacy access_token
+  //   if (!isNone(config.APP.goods.access_token)) {
+  //     return {
+  //       Authorization: `Bearer ${config.APP.goods.access_token}`
+  //     };
+  //   }
+
+  //   let { access_token } = this.get("session.data.authenticated");
+
+  //   if (isNone(access_token)) {
+  //     access_token = config.APP.goods.apiKey;
+  //   }
+  //   return {
+  //     Authorization: `Bearer ${access_token}`,
+  //     "Space-ID": config.APP.goods.spaceId
+  //   };
+  // })
+}
