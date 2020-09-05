6736697ebd20d685c5174fe223b9f41c59270d1e
Kyle Nathan <kyle@wearelion.com>
2020-09-05 15:20:10 +0100

    Remove sku and product services

--- addon/initializers/goods.js
+++ addon/initializers/goods.js
@@ -1,16 +1,12 @@
 import GoodsAdapter from "../adapters/goods";
 import GoodsSerializer from "../serializers/goods";
 import GoodsService from "../services/goods";
-import ProductService from "../services/product";
-import SkuService from "../services/sku";
 
 export function initialize(application) {
   application.register("adapter:application", GoodsAdapter);
   application.register("serializer:application", GoodsSerializer);
 
   application.register("service:goods", GoodsService);
-  application.register("service:product", ProductService);
-  application.register("service:sku", SkuService);
 }
 
 export default {
