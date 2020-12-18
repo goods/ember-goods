1c30f6e2e746c70ec6c1fe704f6f64c40434f154
Kyle Nathan <kyle@wearelion.com>
2020-12-18 10:25:18 +0000

    Add missing product relationship to product-field schema

--- addon/models/product-field.ts
+++ addon/models/product-field.ts
@@ -1,9 +1,11 @@
 import DS from "ember-data";
+import Product from "./product";
 
 export default class ProductField extends DS.Model {
   @DS.attr("string") name!: string;
   @DS.attr("string") slug!: string;
   @DS.attr() values!: any;
+  @DS.belongsTo("product") product!: Product;
 }
 
 declare module "ember-data/types/registries/model" {
