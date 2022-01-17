79cc04fcc296693dcd22888bfc5c04cf16f672c2
Kyle Nathan <kyle@wearelion.com>
2022-01-17 14:49:43 +0000

    Add metaTitle and metaDescription to page

--- addon/models/page.ts
+++ addon/models/page.ts
@@ -4,6 +4,8 @@ import PageTemplate from "./page-template";
 export default class Page extends DS.Model {
   @DS.attr("string") declare name: string;
   @DS.attr("string") declare url: string;
+  @DS.attr("string") declare metaTitle: string;
+  @DS.attr("string") declare metaDescription: string;
   @DS.belongsTo("page-template") declare template: PageTemplate;
 }
 
