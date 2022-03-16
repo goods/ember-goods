3d4ab32d721ab5fe3563472cb9c933cc5760005a
Kyle Nathan <kyle@wearelion.com>
2022-03-16 09:10:23 +0000

    Tidy Order model formatting

--- addon/models/order.ts
+++ addon/models/order.ts
@@ -5,34 +5,38 @@ import OrderLine from "./order-line";
 import OrderPaymentMethod from "./order-payment-method";
 
 export default class Order extends DS.Model {
-  @DS.attr("number", { defaultValue: 0 }) total!: number;
-  @DS.attr("number", { defaultValue: 0 }) quantity!: number;
-  @DS.attr("number", { defaultValue: 0 }) amountPaid!: number;
-  @DS.attr("number", { defaultValue: 0 }) balance!: number;
-  @DS.attr("number", { defaultValue: 0 }) discount!: number;
-  @DS.attr("boolean") marketingOptIn!: boolean;
-  @DS.attr("string") name!: string;
-  @DS.attr("string") emailAddress!: string;
-  @DS.attr("string") phoneNumber!: string;
-  @DS.attr("string") mobileNumber!: string;
-  @DS.attr() metadata!: any;
-  @DS.attr("string") billingAddress1!: string;
-  @DS.attr("string") billingAddress2!: string;
-  @DS.attr("string") billingCity!: string;
-  @DS.attr("string") billingRegion!: string;
-  @DS.attr("string") billingPostcode!: string;
-  @DS.attr("string") billingCountry!: string;
-  @DS.attr("string") shippingAddress1!: string;
-  @DS.attr("string") shippingAddress2!: string;
-  @DS.attr("string") shippingCity!: string;
-  @DS.attr("string") shippingRegion!: string;
-  @DS.attr("string") shippingPostcode!: string;
-  @DS.attr("string") shippingCountry!: string;
-  @DS.belongsTo("basket") basket!: Basket;
+  @DS.attr("string") declare uuid: string;
+  @DS.attr("string") declare sessionId: string;
+  @DS.attr("number", { defaultValue: 0 }) declare total: number;
+  @DS.attr("number", { defaultValue: 0 }) declare quantity: number;
+  @DS.attr("number", { defaultValue: 0 }) declare amountPaid: number;
+  @DS.attr("number", { defaultValue: 0 }) declare balance: number;
+  @DS.attr("number", { defaultValue: 0 }) declare discount: number;
+  @DS.attr("boolean") declare marketingOptIn: boolean;
+  @DS.attr("string") declare name: string;
+  @DS.attr("string") declare emailAddress: string;
+  @DS.attr("string") declare emailConfirmation: string;
+  @DS.attr("string") declare phoneNumber: string;
+  @DS.attr("string") declare mobileNumber: string;
+  @DS.attr() declare metadata: any;
+  @DS.attr("string") declare billingAddress1: string;
+  @DS.attr("string") declare billingAddress2: string;
+  @DS.attr("string") declare billingCity: string;
+  @DS.attr("string") declare billingRegion: string;
+  @DS.attr("string") declare billingPostcode: string;
+  @DS.attr("string") declare billingCountry: string;
+  @DS.attr("string") declare shippingAddress1: string;
+  @DS.attr("string") declare shippingAddress2: string;
+  @DS.attr("string") declare shippingCity: string;
+  @DS.attr("string") declare shippingRegion: string;
+  @DS.attr("string") declare shippingPostcode: string;
+  @DS.attr("string") declare shippingCountry: string;
+  @DS.attr("number") declare shopId: number;
+  @DS.belongsTo("basket") declare basket: Basket;
   @DS.hasMany("order-payment-method")
-  orderPaymentMethods!: OrderPaymentMethod[];
-  @DS.hasMany("payment") payments!: Payment[];
-  @DS.hasMany("order-line") orderLines!: OrderLine[];
+  declare orderPaymentMethods: OrderPaymentMethod[];
+  @DS.hasMany("payment") declare payments: Payment[];
+  @DS.hasMany("order-line") declare orderLines: OrderLine[];
 }
 
 declare module "ember-data/types/registries/model" {
