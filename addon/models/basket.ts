0cd08e87b7dc79fddce8618ce863c63063225cad
Kyle Nathan <kyle@wearelion.com>
2022-03-16 09:08:17 +0000

    Add shopId and sessionId to basket model

--- addon/models/basket.ts
+++ addon/models/basket.ts
@@ -1,12 +1,15 @@
 import DS from "ember-data";
 import BasketItem from "./basket-item";
 import BasketRuleValidation from "./basket-rule-validation";
+import Shop from "./shop";
 
 export default class Basket extends DS.Model {
   @DS.attr("number") total!: number;
   @DS.attr("number") discount!: number;
   @DS.attr("number") balance!: number;
   @DS.attr("number") quantity!: number;
+  @DS.attr("number") shopId!: number;
+  @DS.attr("string") sessionId!: number;
   @DS.attr() metadata!: any;
   @DS.hasMany("basket-item") basketItems!: BasketItem[];
   @DS.hasMany("basket-rule-validation")
