be115a831d0c073ffb1eea1bd5de7f06b5c70753
Kyle Nathan <kyle@wearelion.com>
2022-03-16 09:09:06 +0000

    Add status to payment model

--- addon/models/payment.ts
+++ addon/models/payment.ts
@@ -14,6 +14,7 @@ export default class Payment extends DS.Model {
   @DS.attr("string") issueNumber!: string;
   @DS.attr("string") cvv!: string;
   @DS.attr("string") challengeUrl!: string;
+  @DS.attr("string") status!: string;
   @DS.attr() challengeRequest!: any;
   @DS.attr() challengeResponse!: any;
   @DS.attr("string") challengeSuccessUrl!: string;
