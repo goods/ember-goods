fd1dcc0ab5f62e46ba25feaf5a2ddbb745ca6505
Kyle Nathan <kyle@wearelion.com>
2022-04-14 10:50:15 +0100

    Update to ember 3.28.4

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,12 @@ module.exports = {
       legacyDecorators: true,
     },
   },
-  plugins: ["ember"],
-  extends: ["eslint:recommended", "plugin:ember/recommended"],
+  plugins: ['ember'],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
   env: {
     browser: true,
   },
@@ -20,30 +24,30 @@ module.exports = {
     // node files
     {
       files: [
-        ".eslintrc.js",
-        ".template-lintrc.js",
-        "ember-cli-build.js",
-        "index.js",
-        "testem.js",
-        "blueprints/*/index.js",
-        "config/**/*.js",
-        "tests/dummy/config/**/*.js",
-      ],
-      excludedFiles: [
-        "addon/**",
-        "addon-test-support/**",
-        "app/**",
-        "tests/dummy/app/**",
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
       ],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
       env: {
         browser: false,
         node: true,
       },
-      plugins: ["node"],
-      // extends: ['plugin:node/recommended']
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+    },
+    {
+      // Test files:
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
     },
   ],
 };
