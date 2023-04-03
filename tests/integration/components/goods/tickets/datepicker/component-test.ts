42376ff3037b0d89a696cfa31c2d87cc796da859
Kyle Nathan <kyle@wearelion.com>
2023-04-03 13:17:59 +0100

    Add WIP datepicker

---
+++ tests/integration/components/goods/tickets/datepicker/component-test.ts
@@ -0,0 +1,27 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+// import { render } from '@ember/test-helpers';
+// import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | goods/ticket/datepicker', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders', async function (assert) {
+    assert.ok(true);
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+
+    // await render(hbs`{{goods/ticket/datepicker}}`);
+
+    // assert.equal(this.element.textContent.trim(), '');
+
+    // // Template block usage:
+    // await render(hbs`
+    //   {{#goods/ticket/datepicker}}
+    //     template block text
+    //   {{/goods/ticket/datepicker}}
+    // `);
+
+    // assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+});
