3a7ea7cb3ed1af703159cb6c7c947e7b341f613f
Kyle Nathan <kyle@wearelion.com>
2023-05-25 11:04:00 +0100

    Refactor DayPlanner and PriceCalendar to support ticket packages

--- addon/components/goods/tickets/input/price-calendar.ts
+++ addon/components/goods/tickets/input/price-calendar.ts
@@ -1,5 +1,6 @@
+/* eslint-disable ember/no-side-effects */
 import Component from '@glimmer/component';
-import { isNone } from '@ember/utils';
+import { isNone, isEmpty } from '@ember/utils';
 import { action } from '@ember/object';
 import { inject } from '@ember/service';
 import { dasherize, capitalize } from '@ember/string';
@@ -11,15 +12,34 @@ import { TicketOption } from './ticket';
 import Goods from 'ember-goods/services/goods';
 import Sku from 'ember-goods/models/sku';
 
+export interface TicketPackage {
+  ticketOptions: TicketOption[];
+  confirmationHeading?: string;
+  confirmationMessage?: string;
+  cssClasses?: string[];
+  keyLabel?: string;
+}
+
+type CalendarKeyType = 'price-bands' | 'packages';
+
 interface GoodsTicketsInputPriceCalendarArgs {
+  packages: TicketPackage[];
   selectedTickets: TicketOption[];
+  calendarKeyType: CalendarKeyType;
   startDate: Moment;
   finishDate: Moment;
   onSelect: (date: Date) => void;
 }
 
 export default class GoodsTicketsInputPriceCalendar extends Component<GoodsTicketsInputPriceCalendarArgs> {
+  /**
+   * The store service
+   */
   @inject declare store: Store;
+
+  /**
+   * The goods service
+   */
   @inject declare goods: Goods;
 
   /**
@@ -30,8 +50,27 @@ export default class GoodsTicketsInputPriceCalendar extends Component<GoodsTicke
   /**
    *
    */
+  @tracked isShowingConfirmation: boolean = false;
+
+  /**
+   *
+   */
+  @tracked selectedDay: any = null;
+
+  /**
+   *
+   */
   get isLoading() {
-    return this.skusData.any((data: any) => data.isLoading);
+    return this.packageSkusData.any((dataList: any) =>
+      dataList.any((data: any) => data.isLoading)
+    );
+  }
+
+  /**
+   * Default key type
+   */
+  get calendarKeyType(): CalendarKeyType {
+    return this.args.calendarKeyType ?? 'price-bands';
   }
 
   get hasPriceBand(): boolean {
@@ -52,76 +91,98 @@ export default class GoodsTicketsInputPriceCalendar extends Component<GoodsTicke
   }
 
   get calendarKeys(): any[] {
-    let keys = this.priceBands.map((priceBand) => {
-      return {
-        label: capitalize(priceBand),
-        cssClass: dasherize(priceBand.replace(',', '').toLowerCase()),
-      };
-    });
+    let keys: any = [];
+    if (this.calendarKeyType == 'price-bands') {
+      keys = this.priceBands.map((priceBand) => {
+        return {
+          label: capitalize(priceBand),
+          cssClass:
+            'price-band-' + dasherize(priceBand.replace(',', '').toLowerCase()),
+        };
+      });
+    } else if (this.calendarKeyType == 'packages') {
+      keys = this.args.packages
+        .filter((ticketPackage: TicketPackage) => {
+          return !isNone(ticketPackage.keyLabel);
+        })
+        .map((ticketPackage: TicketPackage) => {
+          return {
+            label: ticketPackage.keyLabel,
+            cssClass: ticketPackage.cssClasses
+              ?.map((cssClass) => 'package-' + cssClass)
+              .join(' '),
+          };
+        });
+    }
 
     return keys;
   }
 
   /**
-   * The calendar SKUs data
+   * The SKUS data for the package
    */
-  #skusCalendarCenter: Moment = moment().utc();
-  #skusData: any = { isLoading: true, value: [] };
-  get skusData() {
+  #packageSkusCalendarCenter: Moment = moment().utc();
+  #packageSkusData: any = { isLoading: true, value: [] };
+  get packageSkusData() {
     let calendarCenter = moment(this.calendarCenter);
 
-    if (!calendarCenter.isSame(this.#skusCalendarCenter)) {
-      this.#skusCalendarCenter = moment(this.calendarCenter);
+    if (!calendarCenter.isSame(this.#packageSkusCalendarCenter)) {
+      this.#packageSkusCalendarCenter = moment(this.calendarCenter);
       let startDate = this.calendarCenter.clone().startOf('month');
       let finishDate = this.calendarCenter.clone().endOf('month');
 
-      let skusData = this.args.selectedTickets.map(
-        (ticketOption: TicketOption) => {
-          let query = [
-            [
-              'bookable-date',
-              'is_same_or_after',
-              startDate.format('YYYY-MM-DDTHH:mm:ss\\Z'),
-            ],
-            [
-              'bookable-date',
-              'is_same_or_before',
-              finishDate.format('YYYY-MM-DDTHH:mm:ss\\Z'),
-            ],
-          ];
+      let packageSkusData = this.args.packages.map(
+        (ticketPackage: TicketPackage) => {
+          return ticketPackage.ticketOptions.map((ticketOption) => {
+            let query = [
+              [
+                'bookable-date',
+                'is_same_or_after',
+                startDate.format('YYYY-MM-DDTHH:mm:ss\\Z'),
+              ],
+              [
+                'bookable-date',
+                'is_same_or_before',
+                finishDate.format('YYYY-MM-DDTHH:mm:ss\\Z'),
+              ],
+            ];
 
-          return load(
-            this.store.query('sku', {
-              filter: {
-                product_id: ticketOption.product.get('id'),
-                query,
-              },
-              page: {
-                offset: 0,
-                limit: 10000,
-              },
-              include: 'price',
-            })
-          );
+            return load(
+              this.store.query('sku', {
+                filter: {
+                  product_id: ticketOption.product.get('id'),
+                  query,
+                },
+                page: {
+                  offset: 0,
+                  limit: 10000,
+                },
+                include: 'price',
+              })
+            );
+          });
         }
       );
 
-      this.#skusData = skusData;
+      this.#packageSkusData = packageSkusData;
     }
 
-    return this.#skusData;
+    return this.#packageSkusData;
   }
 
   /**
-   *
+   * Reduce the entire list of packages and data in a list of loaded SKUs.
+   * This can then be traversed later on to group the SKUs into day availability.
    */
   get skus(): Sku[] {
     if (this.isLoading) {
       return [];
     }
 
-    return this.skusData.reduce((acc: any, data: any) => {
-      return acc.concat(data.value.toArray());
+    return this.packageSkusData.reduce((acc: any, data: any) => {
+      return data.reduce((acc: any, data: any) => {
+        return acc.concat(data.value.toArray());
+      }, acc);
     }, []);
   }
 
@@ -133,65 +194,87 @@ export default class GoodsTicketsInputPriceCalendar extends Component<GoodsTicke
 
     let days = groups.map((group: any) => {
       let date = moment.utc(group.date);
-      let status = 'available';
-      let isSelected = false;
-      let price = 0;
-      // if (!isNone(this.args.selection)) {
-      //   isSelected = date.isSame(this.args.selection.date, 'day');
-      // }
 
-      let hasCapacity = this.args.selectedTickets.every(
-        (ticketOption: TicketOption) => {
-          let productGroup = group.products.findBy(
-            'product.id',
-            ticketOption.product.get('id')
+      let packageCapacities = this.args.packages.map(
+        (ticketPackage: TicketPackage) => {
+          let price = 0;
+          let hasCapacity = ticketPackage.ticketOptions.every(
+            (ticketOption: TicketOption) => {
+              let productGroup = group.products.findBy(
+                'product.id',
+                ticketOption.product.get('id')
+              );
+              if (isNone(productGroup)) {
+                return false;
+              }
+              return this.goods.tickets.hasCapacity(
+                productGroup.skus,
+                ticketOption.ticketTypeOptions
+              );
+            }
           );
-          if (isNone(productGroup)) {
-            return false;
-          }
-          return this.goods.tickets.hasCapacity(
-            productGroup.skus,
-            ticketOption.ticketTypeOptions
+
+          price = ticketPackage.ticketOptions.reduce(
+            (price: number, ticketOption: TicketOption) => {
+              let productGroup = group.products.findBy(
+                'product.id',
+                ticketOption.product.get('id')
+              );
+              if (isNone(productGroup)) {
+                return 0;
+              }
+
+              return (
+                price +
+                this.goods.tickets.getPrice(
+                  productGroup.skus,
+                  ticketOption.ticketTypeOptions
+                )
+              );
+            },
+            0
           );
+
+          return {
+            ticketPackage,
+            hasCapacity,
+            price,
+          };
         }
       );
 
-      if (hasCapacity == false) {
-        status = 'unavailable';
-      }
-
-      price = this.args.selectedTickets.reduce(
-        (price: number, ticketOption: TicketOption) => {
-          let productGroup = group.products.findBy(
-            'product.id',
-            ticketOption.product.get('id')
-          );
-          if (isNone(productGroup)) {
-            return 0;
-          }
-
-          return (
-            price +
-            this.goods.tickets.getPrice(
-              productGroup.skus,
-              ticketOption.ticketTypeOptions
-            )
-          );
-        },
-        0
+      let packageCapacity = packageCapacities.find(
+        (packageCapacity) => packageCapacity.hasCapacity == true
       );
 
-      let priceBand = null;
-      if (!isNone(group.priceBand) && status != 'unavailable') {
-        priceBand = dasherize(group.priceBand.replace(',', '').toLowerCase());
+      let price = 0;
+      let cssClasses = [];
+      let status = 'available';
+
+      if (isNone(packageCapacity)) {
+        status = 'unavailable';
+      } else {
+        price = packageCapacity.price;
+        cssClasses.push(
+          packageCapacity.ticketPackage.cssClasses
+            ?.map((cssClass) => 'package-' + cssClass)
+            .join(' ')
+        );
+
+        if (this.calendarKeyType == 'price-bands' && !isNone(group.priceBand)) {
+          cssClasses.push(
+            'price-band-' +
+              dasherize(group.priceBand.replace(',', '').toLowerCase())
+          );
+        }
       }
 
       return {
         date: date,
         skus: group.skus,
-        priceBand,
+        cssClasses: cssClasses.join(' '),
+        ticketPackage: packageCapacity?.ticketPackage,
         price,
-        isSelected,
         status,
       };
     });
@@ -211,5 +294,19 @@ export default class GoodsTicketsInputPriceCalendar extends Component<GoodsTicke
       return;
     }
     this.calendarCenter = date;
+  }
+
+  /**
+   *
+   * @param day
+   */
+  @action
+  onSelect(day: any) {
+    this.selectedDay = day;
+    if (!isEmpty(day.metadata.ticketPackage.confirmationMessage)) {
+      this.isShowingConfirmation = true;
+    } else {
+      this.args.onSelect(day);
+    }
   }
 }
