83dd9da522b9009510f869d1c762443b5d9238a6
Kyle Nathan <kyle@wearelion.com>
2023-06-27 15:08:01 +0100

    Add combinable to promotion

--- addon/models/promotion.ts
+++ addon/models/promotion.ts
@@ -1,15 +1,16 @@
-import DS from "ember-data";
+import DS from 'ember-data';
 
 export default class Promotion extends DS.Model {
-  @DS.attr("string") name!: string;
-  @DS.attr("string") description!: string;
-  @DS.attr("string") code!: string;
-  @DS.attr("string") startTime!: string;
-  @DS.attr("string") finishTime!: string;
+  @DS.attr('string') name!: string;
+  @DS.attr('string') description!: string;
+  @DS.attr('string') code!: string;
+  @DS.attr('boolean') combinable!: boolean;
+  @DS.attr('string') startTime!: string;
+  @DS.attr('string') finishTime!: string;
 }
 
-declare module "ember-data/types/registries/model" {
+declare module 'ember-data/types/registries/model' {
   export default interface ModelRegistry {
-    "promotion": Promotion;
+    promotion: Promotion;
   }
 }
