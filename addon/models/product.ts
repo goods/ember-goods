2c2b02c579e3be5be2ee0b41769973ee6a7ad2a8
Kyle Nathan <kyle@wearelion.com>
2023-12-01 14:29:16 +0000

    Add taxonomy to product model

--- addon/models/product.ts
+++ addon/models/product.ts
@@ -1,26 +1,27 @@
-import DS from "ember-data";
-import Brand from "./brand";
-import Sku from "./sku";
-import ProductCategory from "./product-category";
-import ShopProductPaymentMethod from "./shop-product-payment-method";
+import DS from 'ember-data';
+import Brand from './brand';
+import Sku from './sku';
+import ProductCategory from './product-category';
+import ShopProductPaymentMethod from './shop-product-payment-method';
 
 export default class Product extends DS.Model {
-  @DS.attr("string") declare name: string;
-  @DS.attr("string") declare summary: string;
-  @DS.attr("string") declare description: string;
-  @DS.attr("string") declare slug: string;
-  @DS.attr("string") declare skuName: string;
-  @DS.belongsTo("brand") declare brand: Brand;
-  @DS.hasMany("sku") declare skus: Sku[];
-  @DS.hasMany("product-category") declare productCategories: ProductCategory[];
-  @DS.hasMany("shop-product-payment-method")
+  @DS.attr('string') declare name: string;
+  @DS.attr('string') declare summary: string;
+  @DS.attr('string') declare description: string;
+  @DS.attr('string') declare slug: string;
+  @DS.attr('string') declare skuName: string;
+  @DS.attr() declare taxonomy: any;
+  @DS.belongsTo('brand') declare brand: Brand;
+  @DS.hasMany('sku') declare skus: Sku[];
+  @DS.hasMany('product-category') declare productCategories: ProductCategory[];
+  @DS.hasMany('shop-product-payment-method')
   declare shopProductPaymentMethods: ShopProductPaymentMethod[];
   @DS.attr() declare attrs: any;
   @DS.attr() declare schema: any;
   @DS.attr() declare skuSchema: any;
 }
 
-declare module "ember-data/types/registries/model" {
+declare module 'ember-data/types/registries/model' {
   export default interface ModelRegistry {
     product: Product;
   }
