5df32e30382633105e47f027044fa998350866c9
Kyle Nathan <kyle@wearelion.com>
2024-04-17 10:17:31 +0100

    Fix free ticket cap check

--- addon/components/goods/tickets/input/visitor.ts
+++ addon/components/goods/tickets/input/visitor.ts
@@ -29,11 +29,10 @@ export default class GoodsTicketsInputVisitor extends Component<GoodsTicketsInpu
    *
    */
   get canIncrement(): boolean {
-    if (this.args.ticketTypeOption.isFree) {
-      return true;
-    }
-
-    if (this.args.canIncrement(this.args.ticketTypeOption) == false) {
+    if (
+      this.args.ticketTypeOption.isFree == false &&
+      this.args.canIncrement(this.args.ticketTypeOption) == false
+    ) {
       return false;
     }
 
